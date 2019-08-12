f983dd53e1f6db73cd75ebbcdbf147ba50b8b608
Serhii Holiak <sholiak@e1z3r5p20.42.us.org>
2019-08-11 18:44:10 -0700

    +++

--- take_input.c
+++ take_input.c
@@ -6,7 +6,7 @@
 /*   By: sholiak <sholiak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 19:31:31 by sholiak           #+#    #+#             */
-/*   Updated: 2019/08/10 20:07:52 by sholiak          ###   ########.fr       */
+/*   Updated: 2019/08/11 18:43:26 by sholiak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int main(int ac, char **av)
 	stack_a = read_digits(stack_a);
 	print_list(stack_a);
 	if(!correct_order(stack_a))
-	return(error("KO"));//push-swap. then OK/KO;
-	return(error("OK"));
+	stack_a = push_swap(stack_a);
+	//error("KO");//push-swap. then OK/KO;
+	error("OK");
+	retunr(0);
 }
