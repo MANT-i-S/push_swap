3112e2527d05b73928082c6dbdc16f2fc8e29247
Serhii Holiak <sholiak@e1z3r5p20.42.us.org>
2019-09-04 16:16:23 -0700

    normed

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: sholiak <sholiak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 12:30:10 by sholiak           #+#    #+#             */
-/*   Updated: 2019/09/04 16:08:34 by sholiak          ###   ########.fr       */
+/*   Updated: 2019/09/04 16:15:30 by sholiak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,11 +61,11 @@ void	checker(t_list *stack_a, t_table *tab, char **digits)
 
 void	ft_use(void)
 {
-	ft_putstr("|ARG=\"replace with digits\"; ./push_swap $ARG | ./checker $ARG|\n");
-	ft_putstr("|To process 2 exe together				     |\n");
-	ft_putstr("|ARG=\"replace with digits\"; ./push_swap $ARG		     |\n");
-	ft_putstr("|To see output of push_swap which suppose to sort stack	     |\n");
-	ft_putstr("|ARG=\"replace with digits\"; ./checker $ARG		     |\n");
-	ft_putstr("|To see if checker works enter comands after request	     |\n");
-	error("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|");
+	ft_putstr("|ARG=\"digits\"; ./push_swap $ARG | ./checker $ARG|\n");
+	ft_putstr("|To process 2 exe together			|\n");
+	ft_putstr("|ARG=\"digits\"; ./push_swap $ARG		        |\n");
+	ft_putstr("|To see output of push_swap which sorts stack   |\n");
+	ft_putstr("|ARG=\"digits\"; ./checker $ARG		        |\n");
+	ft_putstr("|To see if checker works			|\n");
+	error("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|");
 }
